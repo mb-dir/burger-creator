ea8852ac5c2c0f2e1cb676353fb98528ab4a01bb
Michał Bysiewcz <michal.bysiewicz.kontakt@gmail.com>
2024-10-31 17:51:49 +0100

    Create BurgerManager component

---
+++ src/components/BurgerManager.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+	import { computed, ref } from "vue";
+	import type { burgerIngredient } from "@/types";
+	import BurgerSummary from "@/components/BurgerSummary.vue";
+	import BurgerCreator from "@/components/BurgerCreator.vue";
+	import useIngredients from "@/composables/useIngredients";
+
+	const ingredientsError = ref<null | string>(null);
+	const isSavedSuccessfully = ref<boolean>(false);
+
+	const burgerCounter = ref(0);
+
+	// Already added ingredients
+	const burgerIngredients = ref<burgerIngredient[]>([]);
+
+	// Top bun has id 2
+	const isBurgerFinish = computed(() =>
+		burgerIngredients.value.some((el) => el.id === 2)
+	);
+
+	const { ingredients } = useIngredients();
+</script>
+
+<template>
+	<div class="burger-manager">
+		<BurgerCreator
+			:isBurgerFinish
+			:burgerCounter
+			:ingredients
+			v-model:burgerIngredients="burgerIngredients"
+			v-model:ingredientsError="ingredientsError"
+			v-model:isSavedSuccessfully="isSavedSuccessfully"
+		/>
+
+		<BurgerSummary
+			:isBurgerFinish
+			v-model:burgerIngredients="burgerIngredients"
+			v-model:ingredientsError="ingredientsError"
+			v-model:isSavedSuccessfully="isSavedSuccessfully"
+			v-model:burgerCounter="burgerCounter"
+		/>
+	</div>
+</template>
+
+<style scoped>
+	.burger-manager {
+		padding: 16px;
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+	}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,52 +1,10 @@
 <script setup lang="ts">
-	import { computed, ref } from "vue";
-	import type { burgerIngredient } from "@/types";
-	import BurgerSummary from "@/components/BurgerSummary.vue";
-	import BurgerCreator from "@/components/BurgerCreator.vue";
-	import useIngredients from "@/composables/useIngredients";
-
-	const ingredientsError = ref<null | string>(null);
-	const isSavedSuccessfully = ref<boolean>(false);
-
-	const burgerCounter = ref(0);
-
-	const burgerIngredients = ref<burgerIngredient[]>([]);
-
-	const isBurgerFinish = computed(() =>
-		burgerIngredients.value.some((el) => el.id === 2)
-	);
-
-	const { ingredients } = useIngredients();
+	import BurgerManager from "@/components/BurgerManager.vue";
 </script>
 
 <template>
 	<main>
 		<h1>Burger Creator</h1>
-		<div class="grid-wrapper">
-			<BurgerCreator
-				:isBurgerFinish
-				:burgerCounter
-				:ingredients
-				v-model:burgerIngredients="burgerIngredients"
-				v-model:ingredientsError="ingredientsError"
-				v-model:isSavedSuccessfully="isSavedSuccessfully"
-			/>
-
-			<BurgerSummary
-				:isBurgerFinish
-				v-model:burgerIngredients="burgerIngredients"
-				v-model:ingredientsError="ingredientsError"
-				v-model:isSavedSuccessfully="isSavedSuccessfully"
-				v-model:burgerCounter="burgerCounter"
-			/>
-		</div>
+		<BurgerManager />
 	</main>
 </template>
-
-<style scoped>
-	.grid-wrapper {
-		padding: 16px;
-		display: grid;
-		grid-template-columns: 1fr 1fr;
-	}
-</style>
